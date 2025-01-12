89bc3192ea27bc5cff65cbc8e3cb8748b13f6797
luminecs <wangxuke.china@gmail.com>
2025-01-12 12:10:28 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ function MyButton() {
     <button>
       我是一个按钮
     </button>
-  );
+  )
 }
 
 export default function MyApp() {
   return (
     <div>
       <h1>欢迎来到我的应用</h1>
-      <MyButton />
+      <MyButton/>
     </div>
-  );
+  )
 }
